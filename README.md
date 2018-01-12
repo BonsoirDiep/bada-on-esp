0d7ec1eea77afceb7eb29a3346a8d266a7803bef
DiepNguyen <thandieu13@gmail.com>
2018-01-12 12:12:46 +0700

    fix README.md

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Easy IOTs with ESP kit: like NodeMCU
+
+Note:
+```text
+id [ 12, 13, 14, 15, 16 ] -> node with type ao
+id [ 2, 4, 5 ] -> node with type do
+id [ 17 ] -> node with type ai
+
+https://nodemcu.readthedocs.io/en/master/en/modules/gpio/
+```text
+A0-> 17
+D0-> 16
+D1-> 5
+D2-> 4
+D3-> 0
+D4-> 2
+D5-> 14
+D6-> 12
+D7-> 13
+D8-> 15
--- bada-on-esp.ino
+++ bada-on-esp.ino
@@ -4,28 +4,13 @@
 
 // Set these to run example.
 #define FIREBASE_HOST "badaiots-kz.firebaseio.com"
-#define WIFI_SSID "BMTDH3"
-#define WIFI_PASSWORD "0979818125"
+#define WIFI_SSID "y5.cafe"
+#define WIFI_PASSWORD "12345678diepk3r"
 int myPins[] = {0,0,0,0,0}; // toi da 5 cam bien
 int myLights[] = {0,0,0,0,0}; // toi da 5 den
 int myAnalogs[] = {0,0,0,0,0}; // toi da 5 den xoay
 
-void setup() {
-  Serial.begin(9600);
-
-  // connect to wifi.
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("connecting");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(500);
-  }
-  Serial.println();
-  Serial.print("connected: ");
-  Serial.println(WiFi.localIP());
-  
-  Firebase.begin(FIREBASE_HOST);
-  delay(500);
+void getDataDesign(){
   int i = 0;
   String data;
   char str[11];
@@ -34,7 +19,7 @@ void setup() {
   if (Firebase.failed()) {
       Serial.print("load data error: ");
       Serial.println(Firebase.error());
-      return;
+      data = String(";0");
   }
   if(data.length()>0){
     data.toCharArray(str, 11) ;
@@ -56,7 +41,7 @@ void setup() {
   if (Firebase.failed()) {
       Serial.print("load data error: ");
       Serial.println(Firebase.error());
-      return;
+      data = String(";0");
   }
   i = 0;
   if(data.length()>0){
@@ -80,7 +65,7 @@ void setup() {
   if (Firebase.failed()) {
       Serial.print("load data error: ");
       Serial.println(Firebase.error());
-      return;
+      data = String(";0");
   }
   i = 0;
   if(data.length()>0){
@@ -100,41 +85,83 @@ void setup() {
     }
   };
 }
+void setup() {
+  pinMode(D0, OUTPUT);
+  pinMode(D1, OUTPUT);
+  //pinMode(D2, OUTPUT);
+  pinMode(D3, OUTPUT);
+  pinMode(D4, OUTPUT);
+  pinMode(D5, OUTPUT);
+  pinMode(D6, OUTPUT);
+  pinMode(D7, OUTPUT);
+  pinMode(D8, OUTPUT);
+  
+  Serial.begin(9600);
+
+  // connect to wifi.
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("connected: ");
+  Serial.println(WiFi.localIP());
+  
+  Firebase.begin(FIREBASE_HOST);
+  delay(500);
+  getDataDesign();
+}
 
 void loop() {
-  /*int i = 0;
+  bool check = Firebase.getBool("product/hFrPLqQKIbmU/kit/rebuild");
+  if (Firebase.failed()){
+    return;
+  }
+  if(check){ //rebuild
+    Firebase.setBool("product/hFrPLqQKIbmU/kit/rebuild", false);
+    // handle error
+    if (Firebase.failed()) {
+      return;
+    }
+    getDataDesign();
+  }
+  
+  int i = 0;
   for (i = 0; i < 5; i = i + 1) {
     if(myPins[i]>0){
-      Serial.print(myPins[i]);
-      Serial.print("; ");
+      /*
+      float data = Firebase.getFloat("product/hFrPLqQKIbmU/sens/node"+ String(myPins[i]));
+      if (!Firebase.failed()) {
+           //Serial.println(data);
+      }
+      */
+      int analogValue = analogRead(myPins[i]);
+      //float millivolts = (analogValue/1024.0) * 3300; //3300 is the voltage provided by NodeMCU
+      //float celsius = millivolts/10;
+      Firebase.setFloat("product/hFrPLqQKIbmU/sens/node"+ String(myPins[i]), analogValue);
+      delay(200);
     }
   }
-  Serial.println("-------------");
   for (i = 0; i < 5; i = i + 1) {
     if(myLights[i]>0){
-      Serial.print(myLights[i]);
-      Serial.print("; ");
-    }
+      bool data = Firebase.getBool("product/hFrPLqQKIbmU/kit/node"+ String(myLights[i]));
+      if (!Firebase.failed()) {
+          if(data){
+            digitalWrite(myLights[i], HIGH);
+          }
+          else{
+            digitalWrite(myLights[i], LOW);
+          }
+       }
+     }
   }
-  Serial.println("-------------");
   for (i = 0; i < 5; i = i + 1) {
     if(myAnalogs[i]>0){
-      Serial.print(myAnalogs[i]);
-      Serial.print("; ");
+      int data = Firebase.getInt("product/hFrPLqQKIbmU/kit/node"+ String(myAnalogs[i]));
+      if (!Firebase.failed()) analogWrite(myAnalogs[i], (data*1023)/255);
     }
-  }
-  Serial.println("-------------");*/
-  bool data = Firebase.getBool("product/hFrPLqQKIbmU/kit/node1");
-  pinMode(LED_BUILTIN, OUTPUT);
-  if(data){
-    digitalWrite(LED_BUILTIN, LOW);
-  }
-  else digitalWrite(LED_BUILTIN, HIGH);
-  // handle error
-  if (Firebase.failed()) {
-      Serial.print("pushing, getting /logs failed:");
-      Serial.println(Firebase.error());  
-      return;
   }
   delay(200);
 }
