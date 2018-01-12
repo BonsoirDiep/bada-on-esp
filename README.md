8197179e241d11557743c8ad17db9aa3ac26f799
DiepNguyen <thandieu13@gmail.com>
2018-01-12 16:53:44 +0700

    add type i2c

--- README.md
+++ README.md
@@ -1,20 +1,34 @@
 # Easy IOTs with ESP kit: like NodeMCU
+***
+Design [here](https://badaiots-kz.herokuapp.com/nguoidung/thietke.html) (You need "productKey" signup and request productKey (can send mail to: thandieu13@gmail.com))
 
 Note:
 ```text
-id [ 12, 13, 14, 15, 16 ] -> node with type ao
-id [ 2, 4, 5 ] -> node with type do
-id [ 17 ] -> node with type ai
-
-https://nodemcu.readthedocs.io/en/master/en/modules/gpio/
-```text
-A0-> 17
-D0-> 16
-D1-> 5
-D2-> 4
-D3-> 0
-D4-> 2
-D5-> 14
-D6-> 12
-D7-> 13
-D8-> 15
+	id [ 5, 12*, 13*, 14*, 15*, 16* ] 	-> node with type ao (~PWM)
+	id [ 5*, 12, 13, 14*, 15*, 16* ]	-> node with type do
+	id [ 17 ]							-> node with type ai
+	id [ 2, 4 ]							-> node with type i2c
+	
+	(*) different than Arduino Uno
+	
+	To not be confused
+	id [ 5 ] 				-> node with type ao (~PWM)
+	id [ 12, 13 ] 			-> node with type do
+	id [ 17 ] 				-> node with type ai
+	id [ 2, 4 ] 			-> node with type i2c
+```
+[Nodemcu GPIO] (https://nodemcu.readthedocs.io/en/master/en/modules/gpio/)
+```text	
+	D3-> 0
+	D4-> 2
+	D2-> 4
+	
+	A0-> 17
+	
+	D1-> 5
+	D6-> 12
+	D7-> 13
+	D5-> 14
+	D8-> 15
+	D0-> 16
+```
--- bada-on-esp.ino
+++ bada-on-esp.ino
@@ -1,21 +1,54 @@
+// BaDa Software
+// Code for NodeMCU
+// Diep Nguyen - MTA
+
 #include <ESP8266WiFi.h>
 #include <FirebaseArduino.h>
 #include <ESP8266HTTPClient.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 // Set these to run example.
 #define FIREBASE_HOST "badaiots-kz.firebaseio.com"
-#define WIFI_SSID "y5.cafe"
-#define WIFI_PASSWORD "12345678diepk3r"
+#define WIFI_SSID "y5.cafe" // don't mind
+#define WIFI_PASSWORD "12345678diepk3r" // don't mind
+
+//Design i2c max 2, ai max 1...
 int myPins[] = {0,0,0,0,0}; // toi da 5 cam bien
 int myLights[] = {0,0,0,0,0}; // toi da 5 den
 int myAnalogs[] = {0,0,0,0,0}; // toi da 5 den xoay
+int myI2c[] = {0,0,0,0,0}; // toi da 5 cam bien i2c
+String productKey = String("hFrPLqQKIbmU");
 
 void getDataDesign(){
   int i = 0;
   String data;
   char str[11];
+  
+  data = Firebase.getString("product/"+productKey+"/kit/i2c");
+  if (Firebase.failed()) {
+      Serial.print("load data error: ");
+      Serial.println(Firebase.error());
+      data = String(";0");
+  }
+  if(data.length()>0){
+    data.toCharArray(str, 11) ;
+    char * pch;
+    pch = strtok (str,";");
+    while (pch != NULL)
+    {
+      if(i>4) break;
+      int node = atoi(pch);
+      if(node>0){
+         myI2c[i] = node;
+         i++;
+         pch = strtok (NULL, ";");
+      } else break;
+     
+    }
+  };
 
-  data = Firebase.getString("product/hFrPLqQKIbmU/kit/sens");
+  data = Firebase.getString("product/"+productKey+"/kit/sens");
   if (Firebase.failed()) {
       Serial.print("load data error: ");
       Serial.println(Firebase.error());
@@ -37,7 +70,7 @@ void getDataDesign(){
      
     }
   };
-  data = Firebase.getString("product/hFrPLqQKIbmU/kit/lights");
+  data = Firebase.getString("product/"+productKey+"/kit/lights");
   if (Firebase.failed()) {
       Serial.print("load data error: ");
       Serial.println(Firebase.error());
@@ -61,7 +94,7 @@ void getDataDesign(){
     }
   };
 
-  data = Firebase.getString("product/hFrPLqQKIbmU/kit/analogs");
+  data = Firebase.getString("product/"+productKey+"/kit/analogs");
   if (Firebase.failed()) {
       Serial.print("load data error: ");
       Serial.println(Firebase.error());
@@ -88,9 +121,9 @@ void getDataDesign(){
 void setup() {
   pinMode(D0, OUTPUT);
   pinMode(D1, OUTPUT);
-  //pinMode(D2, OUTPUT);
-  pinMode(D3, OUTPUT);
-  pinMode(D4, OUTPUT);
+  //pinMode(D2, OUTPUT); // i2c support
+  // pinMode(D3, OUTPUT);// not-use: GPIO0
+  //pinMode(D4, OUTPUT); // i2c support
   pinMode(D5, OUTPUT);
   pinMode(D6, OUTPUT);
   pinMode(D7, OUTPUT);
@@ -110,17 +143,18 @@ void setup() {
   Serial.println(WiFi.localIP());
   
   Firebase.begin(FIREBASE_HOST);
-  delay(500);
+  delay(900);
   getDataDesign();
+  Firebase.setBool("product/"+productKey+"/kit/rebuild", false);
 }
 
 void loop() {
-  bool check = Firebase.getBool("product/hFrPLqQKIbmU/kit/rebuild");
+  bool check = Firebase.getBool("product/"+productKey+"/kit/rebuild");
   if (Firebase.failed()){
     return;
   }
   if(check){ //rebuild
-    Firebase.setBool("product/hFrPLqQKIbmU/kit/rebuild", false);
+    Firebase.setBool("product/"+productKey+"/kit/rebuild", false);
     // handle error
     if (Firebase.failed()) {
       return;
@@ -130,9 +164,19 @@ void loop() {
   
   int i = 0;
   for (i = 0; i < 5; i = i + 1) {
+    if(myI2c[i]>0){
+	  OneWire oneWire(myI2c[i]);
+	  DallasTemperature sensors(&oneWire);
+	  sensors.begin();
+	  sensors.requestTemperatures();
+      Firebase.setFloat("product/"+productKey+"/sens/node"+ String(myI2c[i]), sensors.getTempCByIndex(0));
+      delay(90);
+    }
+  }
+  for (i = 0; i < 5; i = i + 1) {
     if(myPins[i]>0){
       /*
-      float data = Firebase.getFloat("product/hFrPLqQKIbmU/sens/node"+ String(myPins[i]));
+      float data = Firebase.getFloat("product/"+productKey+"/sens/node"+ String(myPins[i]));
       if (!Firebase.failed()) {
            //Serial.println(data);
       }
@@ -140,13 +184,13 @@ void loop() {
       int analogValue = analogRead(myPins[i]);
       //float millivolts = (analogValue/1024.0) * 3300; //3300 is the voltage provided by NodeMCU
       //float celsius = millivolts/10;
-      Firebase.setFloat("product/hFrPLqQKIbmU/sens/node"+ String(myPins[i]), analogValue);
-      delay(200);
+      Firebase.setFloat("product/"+productKey+"/sens/node"+ String(myPins[i]), analogValue);
+      delay(90);
     }
   }
   for (i = 0; i < 5; i = i + 1) {
     if(myLights[i]>0){
-      bool data = Firebase.getBool("product/hFrPLqQKIbmU/kit/node"+ String(myLights[i]));
+      bool data = Firebase.getBool("product/"+productKey+"/kit/node"+ String(myLights[i]));
       if (!Firebase.failed()) {
           if(data){
             digitalWrite(myLights[i], HIGH);
@@ -159,9 +203,9 @@ void loop() {
   }
   for (i = 0; i < 5; i = i + 1) {
     if(myAnalogs[i]>0){
-      int data = Firebase.getInt("product/hFrPLqQKIbmU/kit/node"+ String(myAnalogs[i]));
+      int data = Firebase.getInt("product/"+productKey+"/kit/node"+ String(myAnalogs[i]));
       if (!Firebase.failed()) analogWrite(myAnalogs[i], (data*1023)/255);
     }
   }
-  delay(200);
+  delay(120);
 }
